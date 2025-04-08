6c409f352e379bebf1912265c993db21471e1c42
citrarrp <citraratup2004@gmail.com>
2025-04-08 14:56:15 +0700

    update Create

--- src/components/form/updateCycle.tsx
+++ src/components/form/updateCycle.tsx
@@ -564,7 +564,8 @@ const UpdateForm = ({
                     className="bg-gray-50 border leading-none border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-emerald-500 focus:border-emerald-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-emerald-500 dark:focus:border-emerald-500"
                     required
                     disabled={
-                      processName.toLowerCase() === "istirahat" && processName.toLowerCase() === "waiting shipping area"
+                      processName.toLowerCase() === "istirahat" 
+                    || processName.toLowerCase() === "waiting shipping area"
                     }
                     onFocus={() => {
                       previousTime.current = field.value || "";
